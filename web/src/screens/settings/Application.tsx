persuaded6414/autobrr

commit b011abe6270a107fd04b921d2d794dcaa5c82ef3

feat(web): truncate long fields

web/src/screens/settings/Application.tsx
@@ -24,7 +24,7 @@ const RowItem = ({ label, value, title, emptyText }: RowItemProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-3 break-all">
+      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-3 break-all truncate">
         {value ? <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value}</span> : emptyText}
       </dd>
     </div>
@@ -60,7 +60,7 @@ const RowItemVersion = ({ label, value, title, newUpdate }: RowItemProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-2 break-all">
+      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-2 break-all truncate">
         <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value}</span>
         {newUpdate && newUpdate.html_url && (
           <span>

web/src/screens/settings/Logs.tsx
@@ -25,7 +25,7 @@ const RowItem = ({ label, value, title, emptyText }: RowItemProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all">
+      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all truncate">
         <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value ? value : emptyText}</span>
       </dd>
     </div>
@@ -43,8 +43,8 @@ const RowItemNumber = ({ label, value, title, unit }: RowItemNumberProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all">
-        <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value}</span>
+      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all truncate">
+        <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow truncate">{value}</span>
         {unit &&
           <span className="ml-1 text-sm text-gray-700 dark:text-gray-400">{unit}</span>
         }

commit f7d5bfb595a7a9553babb1d0fca8905aa8364b2d

fix settings cell item upper/lower corner parts being cut off by 'truncate' (fix for regression introduced in commit b011abe6270a107fd04b921d2d794dcaa5c82ef3)

web/src/screens/settings/Application.tsx
@@ -24,8 +24,8 @@ const RowItem = ({ label, value, title, emptyText }: RowItemProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-3 break-all truncate">
-        {value ? <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value}</span> : emptyText}
+      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-3 break-all">
+        {value ? <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow text-ellipsis leading-7">{value}</span> : emptyText}
       </dd>
     </div>
   );
@@ -60,8 +60,8 @@ const RowItemVersion = ({ label, value, title, newUpdate }: RowItemProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-2 break-all truncate">
-        <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value}</span>
+      <dd className="mt-1 text-gray-900 dark:text-gray-300 text-sm sm:mt-0 sm:col-span-2 break-all">
+        <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow text-ellipsis leading-7">{value}</span>
         {newUpdate && newUpdate.html_url && (
           <span>
             <a href={newUpdate.html_url} target="_blank" rel="noopener noreferrer"><span className="ml-2 inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-sm font-medium text-green-800">{newUpdate.name} available!</span></a>

web/src/screens/settings/Logs.tsx
@@ -25,8 +25,8 @@ const RowItem = ({ label, value, title, emptyText }: RowItemProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all truncate">
-        <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value ? value : emptyText}</span>
+      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all">
+        {value ? <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow text-ellipsis leading-7">{value}</span> : emptyText}
       </dd>
     </div>
   );
@@ -43,8 +43,8 @@ const RowItemNumber = ({ label, value, title, unit }: RowItemNumberProps) => {
   return (
     <div className="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
       <dt className="font-medium text-gray-900 dark:text-white text-sm" title={title}>{label}</dt>
-      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all truncate">
-        <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow truncate">{value}</span>
+      <dd className="mt-1 text-gray-900 dark:text-white text-sm sm:mt-0 sm:col-span-2 break-all">
+        <span className="px-1.5 py-1 bg-gray-200 dark:bg-gray-700 rounded shadow">{value}</span>
         {unit &&
           <span className="ml-1 text-sm text-gray-700 dark:text-gray-400">{unit}</span>
         }